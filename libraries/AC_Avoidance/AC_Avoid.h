7b0f66eeb26cfefcc27c020ec26bea2e52b1901f
liu <liuhanbin913@gmail.com>
2025-10-27 09:28:15 +0800

    add drive

--- libraries/AC_Avoidance/AC_Avoid.h
+++ libraries/AC_Avoidance/AC_Avoid.h
@@ -112,7 +112,8 @@ private:
     // behaviour types (see BEHAVE parameter)
     enum BehaviourType {
         BEHAVIOR_SLIDE = 0,
-        BEHAVIOR_STOP = 1
+        BEHAVIOR_STOP = 1,
+        BEHAVIOR_SMOOTH_STOP = 2
     };
 
     /*
